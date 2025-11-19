f885c54ee6e9055956d296bdfd99a5a87b067ec9
FlameHawkk <74908680+FlameHawkk@users.noreply.github.com>
2025-11-20 02:48:47 +0300

    Delete com_objectdetection directory

--- com_objectdetection/site/objectdetection.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('_JEXEC') or die;
-
-# site/objectdetection.php
-
-$controller = JControllerLegacy::getInstance('Objectdetection');
-$controller->execute(JFactory::getApplication()->input->get('task'));
-$controller->redirect();
